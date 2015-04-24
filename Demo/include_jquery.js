3b262be0f3f90441dfc5a3d00012c20a8be55976
c3h3 <c3h3.tw@gmail.com>
2015-04-24 22:40:46 +0800

    add include jquery codes

---
+++ Demo/include_jquery.js
@@ -0,0 +1,5 @@
+var jq = document.createElement('script');
+jq.src = "http://code.jquery.com/jquery-latest.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+var new_jq = jQuery.noConflict();
+var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
